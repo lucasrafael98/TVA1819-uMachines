7933bcf049a18dcdb0d744d859d5ce4ec1f7d44b
lucasrafael98 <joaolucasrafael@gmail.com>
2018-12-08 14:14:03 +0000

    Increased texture repeat because it looks cooler.

--- uMachines3JS/js/GameElement.js
+++ uMachines3JS/js/GameElement.js
@@ -207,7 +207,7 @@ class GameElement extends THREE.Object3D {
         var texture1 = new THREE.TextureLoader().load(tex_path1);
 		texture1.wrapS = THREE.RepeatWrapping;
 		texture1.wrapT = THREE.RepeatWrapping;
-		texture1.repeat.set(4,4);
+		texture1.repeat.set(10, 10);
 		
         mats1.push(new THREE.MeshBasicMaterial( {color: color, wireframe: wireframe_status, map: texture1, transparent:true, opacity: opacity}));
         mats1.push(new THREE.MeshLambertMaterial( {color: color, wireframe: wireframe_status, map: texture1, transparent:true, opacity: opacity}));
@@ -216,7 +216,7 @@ class GameElement extends THREE.Object3D {
 
         var materials = [mats[mat_option], mats1[mat_option]];
         var mesh = new THREE.SceneUtils.createMultiMaterialObject(geometry, materials);
-        mesh.children[0].position.y -= 0.025;
+        mesh.children[0].position.y -= 0.03;
         for( var i = 0; i != 2; i++){
             mesh.children[i].castShadow = true;
             mesh.children[i].receiveShadow = true;
--- uMachines3JS/shaders/bumpMap.frag
+++ uMachines3JS/shaders/bumpMap.frag
@@ -14,15 +14,15 @@ void main() {
     vec3 lightDir = normalize(L);
     vec3 viewDir = normalize(V);
 
-    vec3 color = texture2D(baseTexture, vUv * 4.0).rgb;
+    vec3 color = texture2D(baseTexture, vUv * 10.0).rgb;
 
-    vec4 normalMapValue = 2.0 * texture2D(normalMap, vUv * 4.0, -1.0) - 1.0;
+    vec4 normalMapValue = 2.0 * texture2D(normalMap, vUv * 10.0, -1.0) - 1.0;
 
     vec3 normal = normalize(normalMapValue.rgb);
 
     vec3 halfDir = normalize(lightDir + viewDir);
 
     float diffuse = max(dot(normal, lightDir), 0.0);
-    gl_FragColor = vec4(diffuse * color, 0.8); 
+    gl_FragColor = vec4(diffuse * color, 0.75); 
 
 }
