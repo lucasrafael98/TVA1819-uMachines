f490c2bc25a2c4c9f5083e52a3119c872e25f7e2
Lucas Rafael <joaolucasrafael@gmail.com>
2018-12-12 09:15:08 +0000

    Centered "Play" button "properly".

--- uMachines3JS/js/Main.js
+++ uMachines3JS/js/Main.js
@@ -293,7 +293,7 @@ function init() {
   mul_height = window.innerHeight * scale * aspect_ratio;
 
   stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-  document.body.appendChild( stats.dom );
+  //document.body.appendChild( stats.dom );
 
   document.body.appendChild(renderer.domElement);
 
